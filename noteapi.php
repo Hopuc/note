c0eeeab4785ed372d4b667b6305cd769158bb1d3
Hopuc <33145408+Hopuc@users.noreply.github.com>
2020-02-02 13:31:35 +0800

    Add files via upload

--- noteapi.php
+++ noteapi.php
@@ -3,14 +3,14 @@
 * Project: Hopuc Note
 * File: noteapi.php
 *
-* @link http://hopuc.con
-* @copyright 2019 Hopuc	
+* @link http://hopuc.com
+* @copyright 2019 Hopuc
 * @author Hopuc <mail@hopuc.com>
-* @version 1.0 
-* @date 2019-04-30
+* @version 1.1
+* @date 2019-12-23
 *
 */
-header('Content-Type:text/html;Charset=utf-8'); 
+header('Content-Type:text/plain;Charset=utf-8'); 
 if(isset($_POST['note'])){
 	$json = json_encode($_POST,320);
 	$file = fopen('note.txt','w+');
@@ -19,6 +19,7 @@ if(isset($_POST['note'])){
 	$reuslt = array(
 	'status' => 1,
 	'message' => '保存成功',
+	'ip' => $_SERVER['REMOTE_ADDR'],
 	'data' => $json,
 	);
 	exit(json_encode($reuslt));
