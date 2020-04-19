240029284ac6de255367c79ac40c3487d8813ab5
hopuc <33145408+hopuc@users.noreply.github.com>
2020-04-19 23:09:28 +0800

    Add files via upload

--- noteapi.php
+++ noteapi.php
@@ -4,27 +4,50 @@
 * File: noteapi.php
 *
 * @link http://hopuc.com
-* @copyright 2019 Hopuc
+* @copyright 2020 Hopuc
 * @author Hopuc <mail@hopuc.com>
-* @version 1.1
-* @date 2019-12-23
+* @version 1.2
+* @date 2020-04-11
 *
 */
-header('Content-Type:text/plain;Charset=utf-8'); 
+header('Content-Type:application/json;Charset=utf-8');
+
+$time=date("Y-m-d H:i:s");
+$ip = $_SERVER['REMOTE_ADDR'];
+
 if(isset($_POST['note'])){
-	$json = json_encode($_POST,320);
-	$file = fopen('note.txt','w+');
-	fwrite($file,$json);
-	fclose($file);
-	$reuslt = array(
-	'status' => 1,
-	'message' => '保存成功',
-	'ip' => $_SERVER['REMOTE_ADDR'],
-	'data' => $json,
+	
+	$content = array(
+		'ip' => $ip,
+		'time' => $time,
+		'note' => $_POST['note'],
 	);
-	exit(json_encode($reuslt));
+	
+	$json = json_encode($content,320);
+	
+	/* $file = fopen('note.json','w+');
+	fwrite($file,$json);
+	fclose($file); */
+	
+	file_put_contents('note.json',$json);
+	
+	$result = array(
+		'success' => true,
+		'message' => '保存成功',
+		'data' => $content,
+	);
+	echo json_encode($result,320);
 }else{
-$json_string = file_get_contents('note.txt');
-echo $json_string;
+	$json_string = @file_get_contents('note.json');
+	if($json_string==null||$json_string==''){
+		$content = array(
+			'ip' => $ip,
+			'time' => $time,
+			'note' => '你还未保存任何内容',
+		);
+		echo json_encode($content,320);
+	}else{
+		echo $json_string;
+	}
 }
 ?>
